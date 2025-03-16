adf8cac0e545c46d0260683d3b3d010a0e158fda
nolan MARSHKE <nmarshke@gmail.com>
2025-03-16 14:19:24 -0400

    added the code for cypress config file'

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,1 +1,21 @@
 import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  component: {
+    devServer: {
+        framework: 'react',
+        bundler: 'vite',
+  },
+
+  specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+},
+
+e2e: {
+    setupNodeEvents: (on, config) => {
+
+    },
+
+    baseUrl: 'http://localhost:3001',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    },
+});
